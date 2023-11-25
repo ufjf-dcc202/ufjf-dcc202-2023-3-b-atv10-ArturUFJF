9fab79ce70abfba9d9f99e45337ccabf78c95998
Artur Ferreira de Castro <arturcastro010@gmail.com>
2023-11-25 18:07:59 -0300

    Modifica arquivo main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { deJoaoParaMaria, deMariaParaJoao, getMaria, getJoao, setMaria, setJoao, maria, joao } from '../src/joaoEMaria.js';
+
+setMaria(3);
+setJoao(2);
+let retornoMaria = getMaria();
+let retornoJoao = getJoao();
+deMariaParaJoao();
+deJoaoParaMaria();
+
